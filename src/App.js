0fdc73a8a2abe36ca48cc251ed4c4af80e667599
Chris Fraser <chrisssfraser@gmail.com>
2020-09-24 22:34:30 +0100

    made outline for a group table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [numberOfPlayersInGroup, setNumberOfPlayersInGroup] = useState(4)
+  const [playersInGroup, setPlayerInGroup] = useState(["chris", "dave", "joni", "Remi"])
+
+  const makeGroupTable = () => {
+    // const numberOfPlayers = playersInGroup.length
+    // playersInGroup.unshift("")
+    console.log(playersInGroup);
+    const headerRow = playersInGroup.map((player) =>{
+      return <div className="group-box">{player}</div>
+    })
+    const mainTable = playersInGroup.map((player) => {
+
+      return (
+        
+      <div className="group-row">
+        <div className="group-box">{player}</div>
+        <div className="group-box"><input className="group-input"></input></div>
+        <div className="group-box"><input className="group-input"></input></div>
+        <div className="group-box"><input className="group-input"></input></div>
+        <div className="group-box"><input className="group-input"></input></div>
+      </div>
+      )
+    })
+    headerRow.unshift(<div className="group-box"></div>)
+    return (
+      <div>
+        <div className="group-row">
+        {headerRow}
+        </div>
+        {mainTable}
+      </div>
+      )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>Header here</p>
       </header>
+
+      {/* <div className="group-box">
+        <p>one</p>
+      </div>
+      <div className="group-box">
+        <p>two</p>
+      </div>
+      <div className="group-box">
+        <p>three</p>
+      </div> */}
+
+      <p>body here</p>
+
+        <div className="group-container">
+        {makeGroupTable()}
+      </div>
+
+
     </div>
   );
 }
